355206b677e5c7d9a08d00d082d9038dd7d39a80
Niha Pereira <niha.pereira@gmail.com>
2017-09-02 17:56:11 -0500

    animated over time stamp

--- harvey_textbot_texts.R
+++ harvey_textbot_texts.R
@@ -13,11 +13,18 @@ library(stringr)
 setwd("~/Harvey_Textbot_Data_Stuff")
 harvey_texts <- read.csv("harvey_texts.csv", stringsAsFactors = FALSE)
 
-##'Get only text bodies with zipcodes mentioned from Texas
+#'Get only text bodies with zipcodes mentioned from Texas
 harvey_texts<- harvey_texts %>% filter(Status == "received")
 pattern <- "(\\b([7][0-9]{4})\\b)"
 texas_zipcodes <- harvey_texts[grepl(pattern, harvey_texts$Body),]
 texas_zipcodes$zip <- str_extract(texas_zipcodes$Body, pattern)
+
+#'Get texts messages inquring about volunteers
+texas_zipcodes$type <- NULL
+texas_zipcodes[,"type"] <- ifelse(grepl("volunteer", texas_zipcodes$Body, ignore.case = TRUE),"volunteer","shelter")
+
+table(texas_zipcodes$type)
+
 
 #'Geocode zipcodes
 data("zipcode")
@@ -40,12 +47,39 @@ texas_base <- ggplot(data = texas_df, mapping = aes(x = long, y = lat, group = g
   coord_fixed(1.3) +
   geom_polygon(color = "black", fill = "gray")
 
+
+library(gganimate)
+
+texas_zipcodes$SentDate <- ymd_hms(texas_zipcodes$SentDate)%>% with_tz(., tzone = "CST6CDT")
+
+
+texas_zipcodes <- texas_zipcodes %>% arrange(SentDate)
+
 texas_counties <- texas_base + theme_nothing() +
   geom_polygon(data = tx_county, fill = NA, color = "white") +
   geom_polygon(color = "black", fill = NA)+
-  geom_point(data = subset(texas_zipcodes, zip %in% c(74000:78000)), aes(x = longitude, y = latitude, group = NULL), color = "red", size = .2) +
+  geom_point(data = subset(texas_zipcodes, zip %in% c(74000:78000)), aes(x = longitude, y = latitude, group = NULL,frame = hour(SentDate), cumulative = TRUE), color = "red") +
   geom_polygon(data = subset(tx_county, subregion == "harris"), fill = "NA", color = "blue", size = 1)
 
 
+gganimate(texas_counties, interval = 2, "output.gif")
+
+# library(leaflet)
+# #pal <- colorFactor(c("red", "green"), domain = c("shelter", "volunteer"))
+# 
+# leaflet(data = texas_zipcodes) %>%
+#   addTiles() %>%
+#   setView(-95.4,29.8, zoom = 10) %>%
+#   addCircleMarkers(
+#     radius = ~ifelse(type == "volunteer",5,2),
+#     color = ~ifelse(type == "volunteer", "green","red"),
+#     stroke = FALSE)
+
+
+#choropleth- text volume per zipcode over time
+
+
+
+###
 
 
